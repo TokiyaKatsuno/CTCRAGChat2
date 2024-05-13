e8b4756bed749dc073fe11a1c325a399670aac88
CTC/Hitomi Iizuka <hitomi.iizuka@ctc-g.co.jp>
2024-05-13 18:10:33 +0900

    旧アプリのファイルをコピー

--- application.py
+++ application.py
@@ -1,17 +1,21 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template
 
+# Flaskアプリケーションを初期化
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def main_page():
-    if request.method == 'GET':
-        text = "ここに結果が出力されます"
-        return render_template("page.html",text=text)
-    elif request.method == 'POST':
-        name = request.form["name"]
-        text = "こんにちは" + name + "さん"
-        return render_template("page.html",text=text)
+# ルートエンドポイントの作成
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-## 実行
-if __name__ == "__main__":
+@app.route('/SimpleChat')
+def simpleChat():
+    return render_template('SimpleChat.html')
+
+@app.route('/AskChat')
+def askChat():
+    return render_template('AskChat.html')
+
+# Run the Flask application
+if __name__ == '__main__':
     app.run(debug=True)
